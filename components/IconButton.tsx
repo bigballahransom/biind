b55f21e0ef3e3f49e9201dc66c9ef83d185b78b3
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2023-07-07 17:05:30 -0700

    moved icon to seperate component

---
+++ components/IconButton.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+import { EvilIcons } from '@expo/vector-icons';
+
+type IconButtonProps = {
+    text?: number | number;
+    icon: React.ComponentProps<typeof EvilIcons>['name'];
+}
+const IconButton = ({icon, text}: IconButtonProps) => {
+    return(
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+                <EvilIcons name={icon} size={22} color='gray' />
+                <Text style={{fontSize: 12, color: 'gray'}}>{text}</Text>
+            </View>
+    )
+}
+
+export default IconButton
--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,19 +1,8 @@
 import { View, Text, Image, StyleSheet } from "react-native";
 import { TweetType } from '../types';
 import { Entypo, EvilIcons } from '@expo/vector-icons';
+import IconButton from './IconButton'
 
-type IconButtonProps = {
-    text?: number | number;
-    icon: React.ComponentProps<typeof EvilIcons>['name'];
-}
-const IconButton = ({icon, text}: IconButtonProps) => {
-    return(
-        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                <EvilIcons name={icon} size={22} color='gray' />
-                <Text style={{fontSize: 12, color: 'gray'}}>{text}</Text>
-            </View>
-    )
-}
 type TweetProps = {
     tweet: TweetType;
 }
